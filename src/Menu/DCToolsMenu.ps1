7710ad8449818316380ad17b3ecccc508180a2db
Pat Cronn <cronnpj@morrisville.edu>
2026-02-11 22:28:39 -0500

    Menu Change

--- src/Menu/DCToolsMenu.ps1
+++ src/Menu/DCToolsMenu.ps1
@@ -4,7 +4,7 @@ Write-Host "-----------------------"
 Write-Host "1) Install AD DS role (no promotion)"
 Write-Host "2) Install DNS role"
 Write-Host "3) Install DHCP role"
-Write-Host "4) Install Core DC roles"
+Write-Host "4) Install Core DC roles (AD DS | DNS | DHCP)"
 Write-Host "0) Back"
 
 $choice = Read-Host "Select an option"
